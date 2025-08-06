8f8be9068b6dc8c661f94cec70a0962fd1c0c184
jairam3525 <jayaramsiddabathula@gmail.com>
2025-08-06 16:46:46 +0530

    Add Eureka Service Registry microservice source code

---
+++ eureka-service-registry/src/main/java/com/hulkhire/eureka_service_registry/hhtp/package-info.java
@@ -0,0 +1,1 @@
+package com.hulkhire.eureka_service_registry.hhtp;
---
+++ eureka-service-registry/src/test/java/com/hulkhire/eureka_service_registry/EurekaServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hulkhire.eureka_service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
